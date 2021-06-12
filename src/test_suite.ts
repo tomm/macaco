db256f39818bd76d0233dd7cc9b8576ea2503f96
Tom Morton <tomm@riseup.net>
2021-06-12 13:24:31 +0100

    Rebuild test db on suite start

--- src/test_suite.ts
+++ src/test_suite.ts
@@ -2,6 +2,7 @@ require("source-map-support").install();
 import user_test from "./backend/commands/macaco_user.test";
 import core_tests from "./backend/macaco_core.test";
 import { sql } from "./backend/macaco_core";
+import * as migrate from "tiny-postgres-migrator";
 
 if (process.env['NODE_ENV'] != 'test') {
   console.error("Error: You must set NODE_ENV=test when running the tests");
@@ -9,6 +10,12 @@ if (process.env['NODE_ENV'] != 'test') {
 }
 
 !(async function() {
+  console.log("Rebuilding test DB...");
+  await sql`drop schema public cascade`;
+  await sql`create schema public`;
+  await migrate.applyAllMigrations(sql, [process.cwd() + '/migrations']);
+
+  console.log("Running test suite...");
   await core_tests.run();
   await user_test.run();
   await sql.end();
